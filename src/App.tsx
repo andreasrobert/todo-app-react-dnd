c9c485f0493a2d5684df52b3f0126cf686cd7f1a
andreasrobert <andreasrobb23@gmail.com>
2021-07-06 21:18:34 +0700

    before dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Post, { Todo } from './components/post';
-import styled from 'styled-components';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-const theme = styled.div`
-
-
-`;
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([] as Todo[]);
+  const [theme, setTheme] = useState("dark")
+
+  if (theme == "light") {
+    document.body.style.backgroundColor = "hsl(236, 33%, 92%)";
+    document.body.style.color = "hsl(236, 9%, 61%)";
+  } else {
+    document.body.style.backgroundColor = "hsl(235, 21%, 11%)";
+    document.body.style.color = "hsl(233, 14%, 35%)";
+  }
 
   const [statusToShow, setStatusToShow] = useState("all" as 'all' | "active" | 'completed')
 
@@ -34,15 +39,22 @@ function App() {
     setInput("");
   }
 
+
+  const onDragEnd = () => {
+
+  }
+
+
+
   return (
     <>
-      <div className="header">
+      <div className={`header ${theme}`}>
         <h1>T O D O</h1>
-        <img id="theme" src="/images/icon-sun.svg" alt="" />
-        <div className="input">
+        <img id="theme" src={theme == "light" ? "/images/icon-moon.svg" : "/images/icon-sun.svg"} alt="" onClick={() => theme === "light" ? setTheme("dark") : setTheme("light")} />
+        <div className={`input ${theme}`}>
           <div className="button"></div>
 
-          <input type="text" value={input} placeholder="Whats your plan for today?" onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)} onChange={(e) => setInput(e.currentTarget.value)} />
+          <input type="text" value={input} placeholder="Whats your plan for today?" onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)} onChange={(e) => setInput(e.currentTarget.value)} className={`${theme}`} />
 
         </div>
 
@@ -50,41 +62,55 @@ function App() {
 
       </div>
 
-      <div className="container">
+      <div className={`container ${theme}`}>
 
+        <DragDropContext onDragEnd={onDragEnd}>
+          
+          <Droppable droppableId="list">
 
-        {todos.filter(todo => {
-          if (statusToShow === "all") { return true }
-          return todo.status === statusToShow
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+              </div>
+            )}
+          </Droppable>
 
-        }).map(todo => (
+            {todos.filter(todo => {
+              if (statusToShow === "all") { return true }
+              return todo.status === statusToShow
 
-          <Post todo={todo} key={todo.id} removePost={postId => setTodos(todos.filter(item => item.id !== postId))}
-            setStatus={(postId, status) => {
-              setTodos(todos.map(item => {
-                if (item.id === postId) {return ({ ...item, status: status })}
-                return item
-              }))
-            }} />
-        ))}
+            }).map(todo => (
 
+              <Post theme={theme} todo={todo} key={todo.id} removePost={postId => setTodos(todos.filter(item => item.id !== postId))}
+                setStatus={(postId, status) => {
+                  setTodos(todos.map(item => {
+                    if (item.id === postId) { return ({ ...item, status: status }) }
+                    return item
+                  }))
+                }} />
+            ))}
+            
+          
+        </DragDropContext>
 
-        <div className="footer">
-          <p id="items">{todos.filter(todo => todo.status === 'active').length} items left</p>
-          <p onClick={() => setStatusToShow("all")} style={{color: statusToShow === 'all' ? 'hsl(220, 98%, 61%)':'hsl(234, 11%, 52%)'}} >All</p>
-          <p onClick={() => setStatusToShow("active")} style={{color: statusToShow === 'active' ? 'hsl(220, 98%, 61%)':'hsl(234, 11%, 52%)'}} >Active</p>
-          <p onClick={() => setStatusToShow("completed")} style={{color: statusToShow === 'completed' ? 'hsl(220, 98%, 61%)':'hsl(234, 11%, 52%)'}} id={"completed"} >Completed</p>
-          <p onClick={() => { setTodos(todos.filter(todo => todo.status !== "completed")) }}>Clear Completed</p>
-        </div>
+          <div className="footer">
+            <p id="items" >{todos.filter(todo => todo.status === 'active').length} items left </p>
+            <p onClick={() => setStatusToShow("all")} style={{ color: statusToShow === 'all' ? 'hsl(220, 98%, 61%)' : 'hsl(234, 11%, 52%)' }} className={`${theme}`} id="all">All</p>
+            <p onClick={() => setStatusToShow("active")} style={{ color: statusToShow === 'active' ? 'hsl(220, 98%, 61%)' : 'hsl(234, 11%, 52%)' }} className={`${theme}`} id="activebar">Active</p>
+            <p onClick={() => setStatusToShow("completed")} style={{ color: statusToShow === 'completed' ? 'hsl(220, 98%, 61%)' : 'hsl(234, 11%, 52%)' }} className={`${theme}`} id="completedbar">Completed</p>
+            <p onClick={() => { setTodos(todos.filter(todo => todo.status !== "completed")) }} className={`${theme}`}>Clear Completed</p>
+          </div>
       </div>
 
-      <h4>Drag and drop to reorder list</h4>
+        <h4>Drag and drop to reorder list</h4>
 
 
 
     </>
-  );
+      );
 }
 
-export default App;
+      export default App;
 
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 export type Todo={id:string,text:string,status:'active' | 'completed'}
 
 
-function Post(props:{ todo:Todo, removePost:(postId:string )=> void, setStatus:( postId:string, status:Todo['status']) => void }) 
+function Post(props:{ theme:string, todo:Todo, removePost:(postId:string )=> void, setStatus:( postId:string, status:Todo['status']) => void }) 
 {
     const { todo } = props;
 
     return (
-        <div className="post">
+        <div className={`post ${props.theme}`}>
             <div className={`button-${todo.status}`} onClick={()=> props.setStatus(todo.id, todo.status === "active" ? "completed" : "active")}>
-                <div className={`circle-${todo.status}`}></div>
+                <div className={`circle-${todo.status} ${props.theme}`}></div>
             </div>
 
-            <h3 id={todo.status} onClick={()=> props.setStatus(todo.id, todo.status === "active" ? "completed" : "active")}>{todo.text}</h3>
+            <h3 id={todo.status} onClick={()=> props.setStatus(todo.id, todo.status === "active" ? "completed" : "active")} className={`${props.theme}`}>{todo.text}</h3>
             <img id="cross" src="/images/icon-cross.svg" alt="" onClick={(e) => props.removePost(todo.id) } />
 
         </div>
